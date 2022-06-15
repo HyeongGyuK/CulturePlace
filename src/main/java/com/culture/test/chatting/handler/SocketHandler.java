fa3e6eb1f5fdfeece85a8cfc5f2e7d97019b4d33
HyeongGyuK <khg3521@naver.com>
2022-06-15 22:34:26 +0900

    websocket 채팅방 생성

--- src/main/java/com/culture/test/chatting/handler/SocketHandler.java
+++ src/main/java/com/culture/test/chatting/handler/SocketHandler.java
@@ -1,6 +1,8 @@
 package com.culture.test.chatting.handler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,7 +17,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class SocketHandler extends TextWebSocketHandler{
 	
 	// 웹소켓 세션을 담아둘 맵
-	HashMap<String, WebSocketSession> sessionMap = new HashMap<>();
+//	HashMap<String, WebSocketSession> sessionMap = new HashMap<>();
+	List<HashMap<String, Object>> rls = new ArrayList<>();
 	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -23,16 +26,46 @@ public class SocketHandler extends TextWebSocketHandler{
 		String msg = message.getPayload();
 		JSONObject obj = jsonToObjectParser(msg);
 		
-		for(String key : sessionMap.keySet()) {
-			WebSocketSession wss = sessionMap.get(key);
+		String rN = (String)obj.get("roomNumber");
+		HashMap<String, Object> temp = new HashMap<String, Object>();
+		
+		if(rls.size() > 0) {
+			for(int i = 0; i < rls.size(); i++) {
+				String roomNumber = (String) rls.get(i).get("roomNumber");
+				if(roomNumber.equals(rN)) {
+					temp = rls.get(i);
+					break;
+				}
+			}
 			
-			try {
-				wss.sendMessage(new TextMessage(obj.toJSONString()));
-			}catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
+			for(String k : temp.keySet()) {
+				if(k.equals("roomNumber")) {
+					continue;
+				}
+				
+				WebSocketSession wss = (WebSocketSession) temp.get(k);
+				
+				if(wss != null) {
+					try {
+						wss.sendMessage(new TextMessage(obj.toJSONString()));
+					} catch (Exception e) {
+						// TODO: handle exception
+						e.printStackTrace();
+					}
+				}
 			}
 		}
+		
+//		for(String key : sessionMap.keySet()) {
+//			WebSocketSession wss = sessionMap.get(key);
+//			
+//			try {
+//				wss.sendMessage(new TextMessage(obj.toJSONString()));
+//			}catch (Exception e) {
+//				// TODO: handle exception
+//				e.printStackTrace();
+//			}
+//		}
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -40,7 +73,35 @@ public class SocketHandler extends TextWebSocketHandler{
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		// 소켓 연결
 		super.afterConnectionEstablished(session);
-		sessionMap.put(session.getId(), session);
+		boolean flag = false;
+		String url = session.getUri().toString();
+		System.out.println(url);
+		
+		String roomNumber = url.split("/chating/")[1];
+		int idx = rls.size();
+		if(rls.size() > 0) {
+			for(int i = 0; i < rls.size(); i++) {
+				String rN = (String)rls.get(i).get("roomNumber");
+				
+				if(rN.equals(roomNumber)) {
+					flag = true;
+					idx = i;
+					break;
+				}
+			}
+		}
+		
+		if(flag) {
+			HashMap<String, Object> map = rls.get(idx);
+			map.put(session.getId(), session);
+		}else {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("roomNumber", roomNumber);
+			map.put(session.getId(), session);
+			rls.add(map);
+		}
+		
+//		sessionMap.put(session.getId(), session);
 		JSONObject obj = new JSONObject();
 		obj.put("type", "getId");
 		obj.put("sessionId", session.getId());
@@ -50,7 +111,13 @@ public class SocketHandler extends TextWebSocketHandler{
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		// 소켓 종료
-		sessionMap.remove(session.getId());
+		if(rls.size() > 0) {
+			for(int i=0; i < rls.size(); i++) {
+				rls.get(i).remove(session.getId());
+			}
+		}
+		
+//		sessionMap.remove(session.getId());
 		super.afterConnectionClosed(session, status);
 	}
 	
