ce745a3b115c7ac06499e53b22afa33d4cfa378b
DESKTOP-BNLDH5C\kiu32 <khg3521@naver.com>
2022-07-02 19:20:10 +0900

    주소 API 중복으로 저장되는 값 수정했습니다. userdetail 만드는 중입니다.

--- src/main/java/com/culture/controller/User/UserController.java
+++ src/main/java/com/culture/controller/User/UserController.java
@@ -1,19 +1,12 @@
 package com.culture.controller.User;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.Valid;
-
+import com.culture.dto.MemberFormDto;
 import com.culture.entity.Member;
-import com.culture.repository.MemberRepository;
-import com.culture.repository.UserCheckRepository;
 import com.culture.service.MemberService;
 import com.culture.service.UserCheckService;
-import com.culture.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.helper.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -22,8 +15,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import com.culture.dto.MemberFormDto;
-import com.culture.dto.UserFormDto;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller(value = "UserController")
 @RequiredArgsConstructor
@@ -91,8 +86,19 @@ public class UserController {
 
    @PostMapping(value = "/new")
    public String joinProc(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+
 	   System.out.println(memberFormDto);
-      
+
+
+	   String address = memberFormDto.getAddress().toString();
+	   String detailAdd = memberFormDto.getDetailAddress().toString();
+
+	   if (!StringUtil.isBlank(detailAdd)){
+		   address += ", " + detailAdd;
+	   }
+
+	   memberFormDto.setAddress(address);
+
 	   if(bindingResult.hasErrors()) {
 		   System.out.println("에러");
  
@@ -106,6 +112,54 @@ public class UserController {
 			   System.out.println("필드 = " + field);
 			   System.out.println("메세지 = " +message);
     
+			   errorMsg.put(field, message);
+		   }
+		   model.addAttribute("errorMsg", errorMsg);
+
+		   return "thymeleaf/member/UsersJoin";
+	   }
+
+	   Member member = Member.createMember(memberFormDto, passwordEncoder);
+	   memberService.saveMember(member) ;
+
+	   return "CulturePlace/main/main";
+   }
+
+
+	@GetMapping(value = "/detail")
+	public String detail(Model model) {
+		model.addAttribute("memberFormDto", new MemberFormDto());
+		return "thymeleaf/member/UsersDetail";
+	}
+
+
+	@PostMapping(value = "/detail")
+   public String datailProc(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+
+	   System.out.println(memberFormDto);
+
+	   String address = memberFormDto.getAddress().toString();
+	   String detailAdd = memberFormDto.getDetailAddress().toString();
+
+	   if (!StringUtil.isBlank(detailAdd)){
+		   address += ", " + detailAdd;
+	   }
+
+	   memberFormDto.setAddress(address);
+
+	   if(bindingResult.hasErrors()) {
+		   System.out.println("에러");
+
+		   List<FieldError> list = bindingResult.getFieldErrors();
+		   Map<String, String> errorMsg = new HashMap<>();
+
+		   for(int i=0;i<list.size();i++) {
+			   String field = list.get(i).getField();
+			   String message = list.get(i).getDefaultMessage();
+
+			   System.out.println("필드 = " + field);
+			   System.out.println("메세지 = " +message);
+
 			   errorMsg.put(field, message);
 		   }
 		   model.addAttribute("errorMsg", errorMsg);
--- src/main/java/com/culture/repository/MemberRepository.java
+++ src/main/java/com/culture/repository/MemberRepository.java
@@ -3,11 +3,10 @@ package com.culture.repository;
 import com.culture.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 //상속 받고 entity 이름 pk 매개변수로 넣어줘야함
 public interface MemberRepository extends JpaRepository<Member, String> {
     // findByEmail: 이메일을 이용하여 회원 검색, 가입시 중복 체크
 //    Member findByEmail(String email);
     Member findByMId(String mId);
+
 }
