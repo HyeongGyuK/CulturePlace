c695b7886741922dcd177686931aa026895bb497
SoonHeonKwon <shkwon93@nate.com>
2022-07-07 12:08:16 +0900

    댓글 삭제까지 완료함

--- src/main/java/com/culture/controller/Board/BoardFreeController/ReplyController.java
+++ src/main/java/com/culture/controller/Board/BoardFreeController/ReplyController.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
 
@@ -37,4 +40,21 @@ public class ReplyController {
         return "redirect:/CommunityMain/board_free_detail/{board_no}";
     }
 
+    // 댓글 삭제 기능
+    @GetMapping(value = "/CommunityMain/board_free_detail/replyDelete")
+    public String doBoardFreeReplyDelete(HttpServletRequest request,
+                                         @RequestParam(value = "reply_no", required = false) Long reply_no) throws Exception{
+
+        if (request.getHeader("Referer") != null) {
+
+            if(reply_no == null){
+                return "redirect:" + request.getHeader("Referer");
+            }else {
+                replyService.boardFreeReplyDelete(reply_no);
+            }
+            return "redirect:" + request.getHeader("Referer");
+        } else {
+            return "redirect:/main";
+        }
+    }
 }
--- src/main/java/com/culture/service/ReplyService.java
+++ src/main/java/com/culture/service/ReplyService.java
@@ -83,4 +83,8 @@ public class ReplyService {
 
 //        return boardFreeReplyRepository.getBoardFreeReplyPage(board_no);
     }
+
+    public void boardFreeReplyDelete(Long reply_no) throws Exception{
+        boardFreeReplyRepository.deleteById(reply_no);
+    }
 }
